5d93d0691457f40270c705b97e410a38f44f6269
calidion <calidion@gmail.com>
2015-08-26 21:11:07 +0800

    enabled mailer

---
+++ cli.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+'use strict';
+var meow = require('meow');
+
+var cli = meow({
+  help: [
+    'Usage',
+    '  notifier [--type mail] [--from name@domain.com] [--to name@domain.com] [--body body] ' +
+    '[--mime (text|html)] ' +
+    '[--host host] [--port port] [--secure secure] [--mail mail] ' +
+    '[--password password] ' +
+    '',
+    'Example',
+    '  notifier --type mail --from admin@sina.com --to admin@sohu.com --body "fuck you!" --mime text'
+  ].join('\n')
+});
+var notifier = require('./index');
+
+var options = notifier.parse(cli.flags);
+
+var type = cli.flags['type'];
+switch(type) {
+  case 'mail':
+  default :
+    var sender = notifier.senders.mailer;
+    sender.send(options.smtp, options.mail, function(error, data) {
+      console.log(error, data);
+    });
+}
--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 'use strict';
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
+module.exports = {
+  senders: {
+    mailer: require('./lib/senders/mail'),
+    sms: require('./lib/senders/sms')
+  },
+
+  parse: function(data) {
+
+    var mailer = this.senders.mailer;
+    if (!data.from) {
+      data.from = data.email;
+    }
+    var smtpOption = mailer.smtp.parse(data);
+    console.log(smtpOption);
+    var mailOption = mailer.content.parse(data);
+    console.log(mailOption);
+
+    return {
+      smtp: smtpOption,
+      mail: mailOption
+    };
+  }
+
 };
